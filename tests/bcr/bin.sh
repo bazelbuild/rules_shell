29b223681f89e8b8b6bf43e8413fb802d177c9e9
maleo <maleo@google.com>
2025-04-28 14:15:21 +0000

    Update tests to use runfiles from rules_shell

--- tests/bcr/bin.sh
+++ tests/bcr/bin.sh
@@ -15,7 +15,7 @@
 
 # --- begin runfiles.bash initialization v3 ---
 # Copy-pasted from the Bazel Bash runfiles library v3.
-set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+set -uo pipefail; set +e; f=rules_shell+/shell/runfiles/runfiles.bash
 # shellcheck disable=SC1090
 source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
   source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
